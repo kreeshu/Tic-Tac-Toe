bd94d38015349a36c411b3849f5a73502c4a11cc
krishu <kreeshukarki1234@gamil.com>
2025-03-27 16:43:46 +0545

    added image when winner is announced

--- index.js
+++ index.js
@@ -1,74 +1,88 @@
-let boxes = document.querySelectorAll(".box");
-let resetButton = document.querySelector("#reset-button");
-let newGameButton = document.querySelector("#new-button"); 
-let msgContainer = document.querySelector(".msg-container"); 
-let msg = document.querySelector("#msg");
+    let boxes = document.querySelectorAll('.box');
+    let msgContainer = document.querySelector('.msg-container');
+    let msg = document.querySelector('#msg');
+    let resetButton = document.querySelector('#reset-button');
+    let newGameButton = document.querySelector("#new-button"); 
+    let winnerImg = document.querySelector('.winner-img img');
 
-let turnO = true;
-const winPatterns = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 4, 5],
-    [6, 7, 8],
-];
+    let turnO = true;
+    winnerImg.style.display = 'none';
+    let winningPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [1, 4, 7], 
+        [2, 5, 8],
+        [0, 4, 8],
+        [0, 3, 6],
+        [2, 4, 6],
+    ];
 
-const resetGame = () => {
-    turnO = true;
-    enabledBoxes();
-    msgContainer.classList.add("hide");
-};
+    // let msgInput = prompt("play game?");
+    // console.log(msgInput); 
 
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        console.log("box was clicked");
-        if (turnO) {
-            box.innerText = "O";
-            turnO = false;
-        } else {
-            box.innerText = "X";
-            turnO = true;
-        }
-        box.disabled = true;
-        checkWinner();
+    boxes.forEach((box) => {
+        box.addEventListener("click", () => {
+            var boxText = box.innerText
+            console.log("box was clicked", boxText);
+            if (turnO) {
+                box.innerText = "O";
+                turnO = false;
+            } else {
+                box.innerText = "X";
+                turnO = true;
+            };
+            box.disabled= true;
+            checkWinner()
+        });
+
     });
-});
 
-const disabledBoxes = () => {
-    for (let box of boxes) {
-        box.disabled = true;  
+    const disabledBoxes = () => {
+        for (let box of boxes) {
+            box.disabled = true;
+        }
+    };
+
+    const enabledBoxes = () => {
+        for (let box of boxes) {
+            box.disabled = false;
+            box.innerText = "";
+        }
+    };
+
+    const resetGame = () => {
+        enabledBoxes()
     }
-};
 
-const enabledBoxes = () => {
-    for (let box of boxes) {
-        box.disabled = false;
-        box.innerText = "";  
-    }
-};
+    const showWinner = (winner) => {
+        msg.innerText= `Congralution your winner ${winner}`;
+        msgContainer.classList.remove('hide')
+        disabledBoxes()
+    2};
 
-const showWinner = (winner) => {
-    msg.innerText = `Congratulations, Winner is ${winner}!`; 
-    msgContainer.classList.remove("hide");
-    disabledBoxes();
-};
+    const checkWinner=()=>{
+        for(let pattern of winningPatterns) {
+            let pos1Value = boxes[pattern[0]].innerText;
+            let pos2Value = boxes[pattern[1]].innerText;
+            let pos3Value = boxes[pattern[2]].innerText;
 
-const checkWinner = () => {
-    for (let pattern of winPatterns) { 
-        let pos1Val = boxes[pattern[0]].innerText;
-        let pos2Val = boxes[pattern[1]].innerText;
-        let pos3Val = boxes[pattern[2]].innerText;
+            console.log(pos1Value, pos2Value, pos3Value)
 
-        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
-            if (pos1Val === pos2Val && pos2Val === pos3Val) { 
-                console.log("winner", pos1Val);
-                showWinner(pos1Val);
-                return; 
+            if(pos1Value !=="" && pos2Value !=="" && pos3Value !=="") {
+                if(pos1Value === pos2Value && pos2Value === pos3Value){
+                    console.log("winner",pos1Value);
+                    showWinner(pos1Value)
+                    // show gif image when winner has been published
+                    winnerImg.style.display = 'block';
+                    // return;
+                }
             }
         }
     }
-}
-newGameButton.addEventListener("click", resetGame);
-resetButton.addEventListener("click", resetGame); 
+
+    newGameButton.addEventListener("click",resetGame);
+    resetButton.addEventListener("click",resetGame);
+
+
+  
